b20dcb3f84c5fed4a861ea9d24a0c775fdea34a4
Andrei-Anaheim <86974515+Andrei-Anaheim@users.noreply.github.com>
2021-08-12 20:31:16 +0300

    add: solve morse decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let code = Object.keys(MORSE_TABLE).map((key) => (key));
+    let codebi1 = []
+    for (let i=0; i<code.length; i+=1) {
+        let codebi = [];
+        for (let j=0; j<code[i].length; j+=1){
+            if (code[i][j] === '-') {
+                codebi.push('11')
+            } else {
+                codebi.push('10')
+            }
+        }
+        codebi1[i] = codebi.toString().replace(/,/g,'').padStart(10,'0');
+    }
+    let letter = Object.keys(MORSE_TABLE).map((key) => MORSE_TABLE[key]);
+    let x = expr.length;
+    let result = [];
+    for (z=0;z<x;z+=10) {
+        let y = expr.slice(z,z+10);
+        result.push(y);
+    }
+    let finalResult = [];
+    for (k=0; k<result.length; k+=1){
+        if (result[k]==='**********') {
+            finalResult.push(' ');
+        } else {
+            let l = codebi1.indexOf(result[k]);
+            finalResult.push(letter[l]);
+        }
+    }
+    return finalResult.join('');
 }
 
 module.exports = {
